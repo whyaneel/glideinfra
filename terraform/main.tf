195bf929492a8c95fa496d7cf2f172ab207ed6ba
Anil G. <whyaneel@gmail.com>
2025-04-08 20:27:26 +0530

    terraform file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,144 @@
+provider "aws" {
+  region = var.region
+}
 
+data "aws_availability_zones" "available" {}
+
+locals {
+  cluster_name = var.cluster_name
+  azs          = slice(data.aws_availability_zones.available.names, 0, 2)
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+    Project     = "glideinfra"
+  }
+}
+
+# Create a random suffix for unique naming
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+# VPC for EKS
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "${local.cluster_name}-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 4, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k + 48)]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  # Tags required for EKS
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
+
+  tags = local.tags
+}
+
+# EKS Cluster
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.15"
+
+  cluster_name    = local.cluster_name
+  cluster_version = var.kubernetes_version
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  # Use spot instances for cost savings
+  eks_managed_node_groups = {
+    main = {
+      name = "node-group-1"
+
+      instance_types = [var.instance_type]
+      capacity_type  = var.use_spot_instances ? "SPOT" : "ON_DEMAND"
+
+      min_size     = var.min_nodes
+      max_size     = var.max_nodes
+      desired_size = var.desired_nodes
+
+      # Enable auto-scaling (adds cost but helps manage resources)
+      create_iam_role = true
+      iam_role_additional_policies = {
+        AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+      }
+
+      tags = {
+        "k8s.io/cluster-autoscaler/enabled"             = "true"
+        "k8s.io/cluster-autoscaler/${local.cluster_name}" = "owned"
+      }
+    }
+  }
+
+  # Manage aws-auth configmap
+  manage_aws_auth_configmap = true
+
+  tags = local.tags
+}
+
+# IAM role for Cluster Autoscaler
+resource "aws_iam_policy" "cluster_autoscaler" {
+  name        = "${local.cluster_name}-cluster-autoscaler"
+  description = "EKS cluster-autoscaler policy for ${local.cluster_name}"
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "autoscaling:DescribeAutoScalingGroups",
+          "autoscaling:DescribeAutoScalingInstances",
+          "autoscaling:DescribeLaunchConfigurations",
+          "autoscaling:DescribeTags",
+          "autoscaling:SetDesiredCapacity",
+          "autoscaling:TerminateInstanceInAutoScalingGroup",
+          "ec2:DescribeLaunchTemplateVersions"
+        ]
+        Resource = "*"
+        Effect   = "Allow"
+      }
+    ]
+  })
+}
+
+# Outputs
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = local.cluster_name
+}
+
+output "region" {
+  description = "AWS region"
+  value       = var.region
+}
+
+output "setup_kubectl" {
+  description = "Configure kubectl command"
+  value       = "aws eks update-kubeconfig --region ${var.region} --name ${local.cluster_name}"
+}
+
+output "cost_optimization_note" {
+  description = "Cost optimization note"
+  value       = "Remember to run ./destroy_glideinfra.sh when you're done to avoid unnecessary charges!"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,53 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "glideinfra-eks"
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version"
+  type        = string
+  default     = "1.28"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for worker nodes"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "min_nodes" {
+  description = "Minimum number of worker nodes"
+  type        = number
+  default     = 1
+}
+
+variable "max_nodes" {
+  description = "Maximum number of worker nodes"
+  type        = number
+  default     = 3
+}
+
+variable "desired_nodes" {
+  description = "Desired number of worker nodes"
+  type        = number
+  default     = 2
+}
+
+variable "use_spot_instances" {
+  description = "Use spot instances for worker nodes (cheaper but can be terminated)"
+  type        = bool
+  default     = true
+}
